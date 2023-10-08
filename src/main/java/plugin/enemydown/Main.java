4844d7dd32c03a3cf3f43c0ce2b7b66488c17b66
Yuki <12kaname111@gmail.com>
2023-10-08 17:29:01 +0900

    DAY12: ②　メモ

--- src/main/java/plugin/enemydown/Main.java
+++ src/main/java/plugin/enemydown/Main.java
@@ -11,8 +11,12 @@ public final class Main extends JavaPlugin implements Listener {
     public void onEnable() {
 
       Bukkit.getPluginManager().registerEvents(this, this);
-
-//この状態でgradleタスクの実行でサーバー
-//      EnablingがEnemyDownになっていることを確認。
+      getCommand("enemyDownStart").setExecutor(new SetLevelCommand());
+//コピーしてくる。それをしないと時間かかる
+//      コマンドまだない。名前もコピーからのものを使いたいわけじゃない
+//      今回回復するコマンドではなく、ゲームをしたい
+//      ゲームの実行コマンド
+//      ゲームスタートだと、何をスタートするのか分からない。
+//      今回はenemydownスタート
     }
 }
