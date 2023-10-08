a776fdd1f956e80a7bd62d98a618a138143d2a9c
Yuki <12kaname111@gmail.com>
2023-10-08 17:03:24 +0900

    DAY12:

--- src/main/java/plugin/enemydown/Main.java
+++ src/main/java/plugin/enemydown/Main.java
@@ -1,16 +1,16 @@
 package plugin.enemydown;
 
-import java.net.http.WebSocket.Listener;
+
 import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin implements Listener {
-//implements入れる
-  @Override
-  public void onEnable() {
-    Bukkit.getPluginManager().registerEvents(this, this);
-//    プラグイン使うから入れる
-  }
+
+    @Override
+    public void onEnable() {
+
+      Bukkit.getPluginManager().registerEvents(this, this);
+
+    }
 }
-
-
