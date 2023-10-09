2f64dbb9a957b59495bbceb7c70350dd34bb85f1
Yuki <12kaname111@gmail.com>
2023-10-10 05:12:36 +0900

    DAY14：武装変更

--- src/main/java/plugin/enemydown/command/EnemydowmCommand.java
+++ src/main/java/plugin/enemydown/command/EnemydowmCommand.java
@@ -12,8 +12,7 @@ import org.bukkit.entity.Player;
 public class EnemydowmCommand implements CommandExecutor {
 
   @Override
-//難しいのは、時間制限＆保存する情報はスコアとプレイヤー名と日時、上書きではなく、すべて保存
-//  保存の情報その次に点数が出る
+//武装や武器はプレイする度に同じになること
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof  Player player) {
       player.setHealth(20);
