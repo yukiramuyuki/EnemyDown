82a6e0048695eec99779965c7c7eb1b7220dbd50
Yuki <12kaname111@gmail.com>
2023-10-10 05:25:52 +0900

    DAY14：武装変更　インベントリ情報を取ってくる。

--- src/main/java/plugin/enemydown/command/EnemydowmCommand.java
+++ src/main/java/plugin/enemydown/command/EnemydowmCommand.java
@@ -14,10 +14,6 @@ public class EnemydowmCommand implements CommandExecutor {
   @Override
 //武装や武器はプレイする度に同じになること
 
-//  上書きするか、他に保存しておいて終わったら元に戻す
-//  今回は強制上書き
-//  対象プレイヤーのイベントリの中身を直接書き換えることで実現する。
-//  コマンド実行時に差し替えt、最終的にはコマンド実行前の装備の状況を保存しておき、ゲーム終了後もとに戻したい。
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof  Player player) {
       player.setHealth(20);
@@ -25,16 +21,26 @@ public class EnemydowmCommand implements CommandExecutor {
       World world = player.getWorld();
       Location playerLocation = player.getLocation();
 
-      double x = playerLocation.getX();
-      double y = playerLocation.getY();
-      double z = playerLocation.getZ();
-
-
-      int random=new SplittableRandom().nextInt(10) + 1;
+      player.getInventory();
+//      インベントリの情報を取ってくる。
 
 
 
-      world.spawnEntity(new Location(world,(x + random)  , y,(z + random) ), EntityType.ZOMBIE);
+
+
+
+
+
+//      double x = playerLocation.getX();
+//      double y = playerLocation.getY();
+//      double z = playerLocation.getZ();
+//
+//
+//      int random=new SplittableRandom().nextInt(10) + 1;
+//
+//
+//
+//      world.spawnEntity(new Location(world,(x + random)  , y,(z + random) ), EntityType.ZOMBIE);
 
 
 
