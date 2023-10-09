694aada28d6dc40273357069b5e89f044339b5c5
Yuki <12kaname111@gmail.com>
2023-10-10 05:20:31 +0900

    DAY14：武装変更　メモ

--- src/main/java/plugin/enemydown/command/EnemydowmCommand.java
+++ src/main/java/plugin/enemydown/command/EnemydowmCommand.java
@@ -13,6 +13,11 @@ public class EnemydowmCommand implements CommandExecutor {
 
   @Override
 //武装や武器はプレイする度に同じになること
+
+//  上書きするか、他に保存しておいて終わったら元に戻す
+//  今回は強制上書き
+//  対象プレイヤーのイベントリの中身を直接書き換えることで実現する。
+//  コマンド実行時に差し替えt、最終的にはコマンド実行前の装備の状況を保存しておき、ゲーム終了後もとに戻したい。
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof  Player player) {
       player.setHealth(20);
