1cb52be4b4f6b56d7d75d06428dc0249536680f1
Yuki <12kaname111@gmail.com>
2023-10-10 05:32:36 +0900

    DAY14：武装変更　sethelmet

--- src/main/java/plugin/enemydown/command/EnemydowmCommand.java
+++ src/main/java/plugin/enemydown/command/EnemydowmCommand.java
@@ -8,6 +8,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
 
 public class EnemydowmCommand implements CommandExecutor {
 
@@ -16,20 +17,20 @@ public class EnemydowmCommand implements CommandExecutor {
 
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof  Player player) {
+      World world = player.getWorld();
+//分けてメモしておく（改行に意味を持たせてみたり）
+//     プレイヤーの状態を初期化。（体力と空腹値を最大に）
       player.setHealth(20);
       player.setFoodLevel(20);
-      World world = player.getWorld();
+
       Location playerLocation = player.getLocation();
 
-      player.getInventory();
-//      インベントリの情報を取ってくる。
+      PlayerInventory inventory = player.getInventory();
+//      変数導入
+      inventory.setHelmet();
+//      sethelmetで上書きできる。
 
-
-
-
-
-
-
+//
 
 //      double x = playerLocation.getX();
 //      double y = playerLocation.getY();
