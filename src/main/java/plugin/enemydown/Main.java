acdbca2b021f94aa7a65ca887da5d8cb06d4abd8
Yuki <12kaname111@gmail.com>
2023-10-10 17:19:50 +0900

    DAY15：点数が手に入る　メモ　二つのEnemydownCommand

--- src/main/java/plugin/enemydown/Main.java
+++ src/main/java/plugin/enemydown/Main.java
@@ -9,15 +9,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 import plugin.enemydown.command.EnemydowmCommand;
 
 public final class Main extends JavaPlugin  {
-//implementsListenerいらない
+
 
     @Override
     public void onEnable() {
 
       Bukkit.getPluginManager().registerEvents(new EnemydowmCommand(), this);
-//      ここの行いらない。なぜならイベントをここでは受け取っていないから！！
+
       getCommand("enemyDown").setExecutor(new EnemydowmCommand());
-//enemydowncommandをthisに
+//newが二つあると、登録されたもの、実際に動くものべつもの
+//      インスタンスという考え方
+
+//      enemydownに基づいてインスタンス（設計書のような）が作られる。
+//      newしたら別々になるそれぞれA,B EnemydownCommand
+//      実際登録されても、違うものが動いてしまうことも
+//      →一時変数で受け取るを実行
     }
 
 }
