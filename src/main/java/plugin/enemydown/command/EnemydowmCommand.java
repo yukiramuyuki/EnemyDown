18975a4cf1f4424770579f35d2e9587ab6a1434f
Yuki <12kaname111@gmail.com>
2023-10-10 13:10:37 +0900

    DAY14：武装変更　済

--- src/main/java/plugin/enemydown/command/EnemydowmCommand.java
+++ src/main/java/plugin/enemydown/command/EnemydowmCommand.java
@@ -20,7 +20,7 @@ public class EnemydowmCommand implements CommandExecutor {
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof  Player player) {
       World world = player.getWorld();
-//分けてメモしておく（改行に意味を持たせてみたり）
+
 //     プレイヤーの状態を初期化。（体力と空腹値を最大に）
       player.setHealth(20);
       player.setFoodLevel(20);
@@ -29,31 +29,30 @@ public class EnemydowmCommand implements CommandExecutor {
 
       PlayerInventory inventory = player.getInventory();
       inventory.setHelmet(new ItemStack(Material.DIAMOND_HELMET));
-//      itemstackでmaterialを指定。
+
       inventory.setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
-//      コピーして種類を変えるだけ。
+
       inventory.setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
       inventory.setBoots(new ItemStack(Material.DIAMOND_BOOTS));
 
       inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_SWORD));
-//      武器はinmainhand（使い方注意）下のバーのところ。
-//      持っているものを上書きする
 
 
 
 
 
 
-//      double x = playerLocation.getX();
-//      double y = playerLocation.getY();
-//      double z = playerLocation.getZ();
-//
-//
-//      int random=new SplittableRandom().nextInt(10) + 1;
-//
-//
-//
-//      world.spawnEntity(new Location(world,(x + random)  , y,(z + random) ), EntityType.ZOMBIE);
+
+      double x = playerLocation.getX();
+      double y = playerLocation.getY();
+      double z = playerLocation.getZ();
+
+
+      int random=new SplittableRandom().nextInt(10) + 1;
+
+
+
+      world.spawnEntity(new Location(world,(x + random)  , y,(z + random) ), EntityType.ZOMBIE);
 
 
 
