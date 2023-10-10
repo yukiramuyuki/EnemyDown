263bdcc680f2959141fd9daa6eaca0ceade8ad9f
Yuki <12kaname111@gmail.com>
2023-10-10 14:09:22 +0900

    DAY15：点数が手に入る　DeathEventイベントハンドラー

--- src/main/java/plugin/enemydown/command/EnemydowmCommand.java
+++ src/main/java/plugin/enemydown/command/EnemydowmCommand.java
@@ -9,6 +9,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
@@ -16,34 +18,18 @@ public class EnemydowmCommand implements CommandExecutor {
 
   @Override
 //敵を倒すと点数が手に入ること
-//種類によって点数が違うは後でする。
-//  敵を倒した状態を取得する
 
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof  Player player) {
       World world = player.getWorld();
+      Location playerLocation = player.getLocation();
 
 //     プレイヤーの状態を初期化。（体力と空腹値を最大に）
       player.setHealth(20);
       player.setFoodLevel(20);
 
-      Location playerLocation = player.getLocation();
 
-      PlayerInventory inventory = player.getInventory();
-      inventory.setHelmet(new ItemStack(Material.DIAMOND_HELMET));
-
-      inventory.setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
-
-      inventory.setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
-      inventory.setBoots(new ItemStack(Material.DIAMOND_BOOTS));
-
-      inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_SWORD));
-
-
-
-
-
-
+      //ゾンビ出現させる
 
       double x = playerLocation.getX();
       double y = playerLocation.getY();
@@ -58,10 +44,28 @@ public class EnemydowmCommand implements CommandExecutor {
 
 
 
+//プレイヤーの武装
+      PlayerInventory inventory = player.getInventory();
+      inventory.setHelmet(new ItemStack(Material.DIAMOND_HELMET));
+
+      inventory.setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
+
+      inventory.setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
+      inventory.setBoots(new ItemStack(Material.DIAMOND_BOOTS));
+
+      inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_SWORD));
+
+
 
     }
     return false;
   }
-
+  @EventHandler
+  public void onEnemyDeth(EntityDeathEvent e){
+    //      EntityDeathEventがある
+//      イベントハンドラーで取得する
+  }
 
 }
+
+
