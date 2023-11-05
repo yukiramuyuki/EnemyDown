a216d26852104b762fc8e84a76ef2bb81da634ab
Yuki <12kaname111@gmail.com>
2023-11-05 19:03:36 +0900

    Day16:マルチプレイ　メモ　フィールド情報持ちすぎない

--- src/main/java/plugin/enemydown/data/PlayerScore.java
+++ src/main/java/plugin/enemydown/data/PlayerScore.java
@@ -3,13 +3,16 @@ package plugin.enemydown.data;
 import org.bukkit.entity.Player;
 
 /**
- * EnemyDownのゲームを実行する際のスコア情報を扱うオブジェクト。
- * プレイヤー名、合計点数、日時などの情報をもつ。
+ * EnemyDownのゲームを実行する際のスコア情報を扱うオブジェクト。 プレイヤー名、合計点数、日時などの情報をもつ。
  */
 
 public class PlayerScore {
-  //フィールドの情報を持ってくる（enemydowncommandからコピーで）
-  private Player player;
+
+  private String playerName;
+//  enemydownでplayerを使いたいわけではなくplayernameを使いたい
+//  playerをStringにしてplayernameでも
+//  playerたくさん情報持っていて便利だが、必要なものに絞る
+//  ここのクラスでもっておきたいもの。項目は絞る
   private int score;
 
 }
