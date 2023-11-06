ca95e023651b6b0948b2b20fa8b121109f110a1f
Yuki <12kaname111@gmail.com>
2023-11-06 13:53:08 +0900

    Day16:マルチプレイ　Lombokなしでやる

--- src/main/java/plugin/enemydown/data/PlayerScore.java
+++ src/main/java/plugin/enemydown/data/PlayerScore.java
@@ -6,13 +6,9 @@ import org.bukkit.entity.Player;
 /**
  * EnemyDownのゲームを実行する際のスコア情報を扱うオブジェクト。 プレイヤー名、合計点数、日時などの情報をもつ。
  */
+//Lombokできないが、新しくプロジェクトでしたらできた！！
 
 public class PlayerScore {
-//  2項目なのに行が増える！！
-//  build.gradle→conpileonlyにテキストの追加コードを貼り付ける
-
-//  Lombokが使えるようになる
-
 
 
   private String playerName;
