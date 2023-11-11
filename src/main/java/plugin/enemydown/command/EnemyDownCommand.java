3f1ede212ed4308f62b260aa717aa736c6028cf3
Yuki <12kaname111@gmail.com>
2023-11-11 16:34:48 +0900

    Day:19種類で点数変動　メモ

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -44,9 +44,6 @@ public class EnemyDownCommand implements CommandExecutor, Listener {
 
       nowPlayer.setGameTime(20);
 
-
-
-
       World world = player.getWorld();
 
 //装備など設定
@@ -54,16 +51,17 @@ public class EnemyDownCommand implements CommandExecutor, Listener {
 
 //ゾンビを出現させる
 
-
-//      Day18スコア表示
+//      Day19：敵の種類によって手に入る点数が異なること
+//      倒した敵によって種類を分岐させて、点数を変動させる
+//      スコアの点数実装済みその部分の処理を分岐させるだけ
+//      ifを使って？
 
       Bukkit.getScheduler().runTaskTimer(main, Runnable -> {
         if (nowPlayer.getGameTime() <= 0) {
           Runnable.cancel();
           player.sendTitle("ゲームが終了しました。",
-              nowPlayer.getPlayerName()+" 合計"+nowPlayer.getScore()+"点！",
-              0,30,0);
-
+              nowPlayer.getPlayerName() + " 合計" + nowPlayer.getScore() + "点！",
+              0, 30, 0);
 
           nowPlayer.setScore(0);
 
