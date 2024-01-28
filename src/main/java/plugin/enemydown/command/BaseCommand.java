38e501d5378ce00904513248524b926ce318b608
Yuki <12kaname111@gmail.com>
2024-01-28 17:34:23 +0900

    Day:25 赤波解消

--- src/main/java/plugin/enemydown/command/BaseCommand.java
+++ src/main/java/plugin/enemydown/command/BaseCommand.java
@@ -2,7 +2,6 @@ package plugin.enemydown.command;
 //  Day21onCommandなくす
 
 
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,19 +11,16 @@ import org.bukkit.entity.Player;
  * コマンドを実行して動かすプログイン処理の基底クラスです。
  */
 public abstract class BaseCommand implements CommandExecutor {
-  //  コマンドのオプションでプレイヤーの強さ、敵の種類をある程度コントロールできる
-//  難易度を変える（敵の種類を変える）
-//  enemydownに引数を設定できる必要がある
 
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player) {
-     return onExecutePlayerCommand(player,command,label,args);
-//     senderのみある。引数受けられなくなっている。ベースコマンドのバグ。コマンド、ラベル、アーグスをきちんとわたす。
+      return onExecutePlayerCommand(player, command, label, args);
+//     赤波ベースコマンドにメゾットonExecutePlayerCommandを作成とでるがonExecutePlayerCommandを変更する
 
-    }else {
-      return onExecuteNPCCommand(sender,command,label,args);
+    } else {
+      return onExecuteNPCCommand(sender, command, label, args);
     }
 
 
@@ -32,19 +28,27 @@ public abstract class BaseCommand implements CommandExecutor {
 
   /**
    * コマンド実行者がプレイヤーだった場合に実行します。
-   * @param player コマンドを実行したプレイヤー
+   *
+   * @param player  コマンドを実行したプレイヤー
+   * @param command コマンド
+   * @param label   ラベル
+   * @param args    コマンド引数
    * @return 処理の実行有無
    */
 
-  public abstract boolean onExecutePlayerCommand(Player player) ;
+  public abstract boolean onExecutePlayerCommand(Player player, Command command, String label,
+      String[] args);
+//  コピーして貼り付け
 
   /**
    * コマンドの実行者がプレイヤー意外だった場合に実行します。
+   *
    * @param sender コマンド実行者
    * @return 処理の実行有無
    */
-  public abstract boolean onExecuteNPCCommand(CommandSender sender) ;
-
+  public abstract boolean onExecuteNPCCommand(CommandSender sender, Command command, String label,
+      String[] args);
+//コピーして貼り付け
 
 }
 
