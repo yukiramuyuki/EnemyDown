c41e55ba552905a04c0c9199dab5fd6ca789d683
Yuki <12kaname111@gmail.com>
2024-06-05 21:53:11 +0900

    DAY31；クラスの中身さくせい＆Getter/Setter

--- src/main/java/plugin/enemydown/mapper/data/PlayerScore.java
+++ src/main/java/plugin/enemydown/mapper/data/PlayerScore.java
@@ -1,11 +1,65 @@
-//クラスを作成する場所はdataの中でもいい。
-//  こうしなければならないというよりは、見やすい。分かりやすい。を優先した。
-//  パッケージを作ってその中に入れた
 
 package plugin.enemydown.mapper.data;
-//PlayerScoreの中には何があるか
-//getでとれる項目を
 
+/**
+ * プレイヤーのスコア情報を扱うオブジェクト。 DBに存在するテーブルと連動する。
+ */
+
+/**
+ * @Getter&@Setter
+ */
 public class PlayerScore {
+
+  private int id;
+  private String playerName;
+  private int score;
+  private String difficulty;
+  private String registered_at;
+  //  日付文字列でとってきてるからString
+
+
+  /**
+   *生成でGetter/Setter
+   * @return
+   */
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getPlayerName() {
+    return playerName;
+  }
+
+  public void setPlayerName(String playerName) {
+    this.playerName = playerName;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+
+  public String getDifficulty() {
+    return difficulty;
+  }
+
+  public void setDifficulty(String difficulty) {
+    this.difficulty = difficulty;
+  }
+
+  public String getRegistered_at() {
+    return registered_at;
+  }
+
+  public void setRegistered_at(String registered_at) {
+    this.registered_at = registered_at;
+  }
 
 }
